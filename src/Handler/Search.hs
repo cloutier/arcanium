d8b5bb927d9d6837840ff4bbe1d5cf4acde75043
Vincent Cloutier <vincent@cloutier.co>
2017-08-12 15:07:13 -0400

    mongo logging mostly working

--- src/Handler/Search.hs
+++ src/Handler/Search.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE QuasiQuotes #-}
@@ -10,6 +12,23 @@ import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 import Text.Julius (RawJS (..))
 import Text.Regex.Posix
+import Database.Persist 
+import Database.Persist.TH
+import Database.Persist.MongoDB 
+import Language.Haskell.TH.Syntax
+import Network (PortID (PortNumber))
+import qualified Data.ByteString.Lazy.Char8 as Char8 (unpack)
+import Data.Text.Encoding
+
+
+
+let mongoSettings = (mkPersistSettings (ConT ''MongoContext)) {mpsGeneric = False}
+  in share [mkPersist mongoSettings] [persistLowerCase| 
+Queries
+    terms String
+    ip Text Maybe
+    deriving (Show)
+|]
 
 -- Define our data that will be used for creating the form.
 data FileForm = FileForm
@@ -26,9 +45,22 @@ data FileForm = FileForm
 -- inclined, or create a single monolithic file.
 getSearchR :: Handler ()
 getSearchR = do
+	maybeIp <- lookupHeader "X-Real-IP"
+	let ip = fmap (decodeASCII) maybeIp
 	let searchEngine = "https://duckduckgo.com/?q=" :: String
 	searchTermsMaybe <- lookupGetParams "q"
 	let searchTerms = intercalate " " $ map unpack searchTermsMaybe
+	withMongoDBConn
+	    "test"
+	    "localhost"
+	    (PortNumber 27017)
+	    Nothing
+	    2000
+	    (runMongoDBPool
+		master
+		(do user <- insert $ Queries searchTerms ip
+		    liftIO $ print user
+		    return ()))
         let searchEngine2 = findBangs searchTerms
         let searchEngine3 = fmap (++ searchTerms) searchEngine2
 	case searchEngine3 of Just x -> redirect (x :: String)
